83c05a8b38285c3768a7c84b339a389bc7f55ae6
sfrick <sebastian.frick@fincon.eu>
2021-05-20 10:04:48 +0200

    Add gh action auto deploy

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
   <div id="app">
-    Hello F1Rennfreunde!
+<!--    <b-image src="logo-rennfreunde.png" alt="logo-rennfreunde"/>-->
+
+    <text-animation>F1 Rennfreunde</text-animation>
   </div>
 </template>
 
 <script>
 
+// import BImage from "@/components/base/img/BImage";
+import TextAnimation from "@/components/base/TextAnimation";
 export default {
   name: 'App',
-  components: {
-  }
+  components: {TextAnimation},
+  // components: {
+  //   BImage
+  // }
 }
 </script>
 
 <style>
+:root{
+  --primary-color: rgb(241, 144, 49);
+  --primary-color-lighten: rgb(252, 205, 165);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: hsl(0, 0%, 100%);
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+
+  background-color: rgba(45,45,45,0.95);
+}
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background: url("assets/F12020.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const path = require('path')
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/site/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/site/' : '/',
+    // chainWebpack: config => {
+    //     config.resolve.alias
+    //         .set('assets', path.resolve('assets', 'src/assets'))
+    //         .set('@', path.resolve('src'))
+    // }
 }
